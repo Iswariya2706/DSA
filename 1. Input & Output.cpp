97e705065372d632420291912e7e327e9ae7d25a
Iswariya2706 <iswariya438@gmail.com>
2024-12-01 22:14:16 +0530

    Add files via upload

---
+++ 1. Input & Output.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a;
+    cout<<"Enter a number:";
+    cin>> a;
+    cout<< a << endl;
+    string s;
+    cout<<"Enter a word:";
+    cin>> s;
+    cout<< s << endl;
+    char x;
+    cout<<"Enter a letter:";
+    cin>> x;
+    cout<< x;
+
+    return 0;
+}
---
+++ 2. Triangle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    
+    cin>> a;
+    cin>> b;
+    cin>> c;
+    
+    if (a+b+c==180){
+         cout<<"It is a triangle";
+    } else {
+        cout<<"It is not a triangle";
+    }
+     
+      
+
+    return 0;
+}
